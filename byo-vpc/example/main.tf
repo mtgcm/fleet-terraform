364e3a1327ad7a3bedbd225ae75835be34870df1
George Karr <georgekarrv@users.noreply.github.com>
2025-10-01 14:50:10 -0500

    Bumping version (#114)

--- byo-vpc/example/main.tf
+++ byo-vpc/example/main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 }
 
 locals {
-  fleet_image = "fleetdm/fleet:v4.73.3"
+  fleet_image = "fleetdm/fleet:v4.73.4"
   domain_name = "example.com"
 }
 
--- variables.tf
+++ variables.tf
@@ -230,7 +230,7 @@ variable "fleet_config" {
     mem                          = optional(number, 4096)
     cpu                          = optional(number, 512)
     pid_mode                     = optional(string, null)
-    image                        = optional(string, "fleetdm/fleet:v4.73.3")
+    image                        = optional(string, "fleetdm/fleet:v4.73.4")
     family                       = optional(string, "fleet")
     sidecars                     = optional(list(any), [])
     depends_on                   = optional(list(any), [])
@@ -370,7 +370,7 @@ variable "fleet_config" {
     mem                          = 512
     cpu                          = 256
     pid_mode                     = null
-    image                        = "fleetdm/fleet:v4.73.3"
+    image                        = "fleetdm/fleet:v4.73.4"
     family                       = "fleet"
     sidecars                     = []
     depends_on                   = []
